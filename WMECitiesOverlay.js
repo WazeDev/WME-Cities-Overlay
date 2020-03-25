eeed75f93089384f20002d96699a2bf6b6210da4
justins83 <justins83.waze@gmail.com>
2020-03-25 15:47:29 -0400

    Update WMECitiesOverlay.js

--- WMECitiesOverlay.js
+++ WMECitiesOverlay.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Cities Overlay
 // @namespace    https://greasyfork.org/en/users/166843-wazedev
-// @version      2019.11.21.01
+// @version      2020.03.25.01
 // @description  Adds a city overlay for selected states
 // @author       WazeDev
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
